30aefd62372f22f773940b6de9046e4f5155d33d
Jonczyk <krzysztof.jonczyk@goldenline.pl>
2016-07-07 18:48:44 +0200

    bower + saving token

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,49 +2,64 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Auth;
 use EDAM\NoteStore\NoteFilter;
 use EDAM\NoteStore\NoteList;
-use EDAM\NoteStore\NoteMetadata;
-use EDAM\NoteStore\NotesMetadataList;
 use EDAM\NoteStore\NotesMetadataResultSpec;
-use EDAM\Types\Note;
 use Evernote\AdvancedClient;
 use Evernote\Auth\OauthHandler;
 use Evernote\Client;
-use Evernote\Model\Notebook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $oauthHandler = new OauthHandler(true);
+        $auth = $this->getDoctrine()->getRepository(Auth::class)->find(1);
+        if (true === $auth instanceof Auth) {
+            return $this->redirectToRoute("schedule");
+        }
+
         $key = $this->getParameter("en_key");
         $secret = $this->getParameter("en_secret");
 
-//        $oauthData = $oauthHandler->authorize($key, $secret, "vendor/evernote/evernote-cloud-sdk-php/sample/oauth/index.php");
+        $oauthHandler = new OauthHandler(true, false, false);
+        $tokenData = $oauthHandler->authorize($key, $secret, $this->generateUrl("homepage", [], UrlGeneratorInterface::ABSOLUTE_URL));
 
-//        $this->get("session")->set("en_token", $oauthData['oauth_token']);
+        if (false === empty($tokenData)) {
+            $token = $tokenData["oauth_token"];
+            $this->get("session")->set("en_token", $token);
 
-        // replace this example code with whatever you need
-        return $this->redirectToRoute("schedule");
+            $em = $this->getDoctrine()->getEntityManager();
+
+            $auth = new Auth();
+            $auth
+                ->setToken($token)
+                ->setCreatedAt(new \DateTime());
+
+            $em->persist($auth);
+            $em->flush();
+
+            return $this->redirectToRoute("schedule");
+        }
+
+        exit;
     }
 
     /**
      * @Route("/schedule/", name="schedule")
-     * @Template(":default:schedule.html.twig")
      *
      * @param Request $request
      *
      * @return array
      */
-    public function scheduleAction(Request $request)
+    public function scheduleAction()
     {
         $token = $this->get("session")->get("en_token");
 
@@ -52,26 +67,22 @@ class DefaultController extends Controller
 
         $client = new Client($token, true, $advancedClient);
 
-
         $nFilter = new NoteFilter();
         $nFilter->words = "reminderTime:*";
         $nFilter->order = "created";
         $rSpec = new NotesMetadataResultSpec();
         $rSpec->includeTitle = true;
         $rSpec->includeAttributes = true;
-//        $rSpec->includeTitle = true;
+        $rSpec->includeTitle = true;
 
-        $notes = $client->getUserNotestore()->findNotesMetadata($token, $nFilter, 0, 50, $rSpec);
+        try {
+            $noteList = $client->getUserNotestore()->findNotesMetadata($client->getToken(), $nFilter, 0, 50, $rSpec);
+        } catch (\Exception $e) {
+            $t = $e;
+        }
 
-//        $filter = new NoteFilter();
-//        $filter->notebookGuid = "03db77d0-8e9d-41a3-8666-ad3b303df946";
-//
-//        $notes = new NoteMetadata($filter);
-
-        /** @var NoteList $notes */
-//        $notes = $client->getUserNotestore()->findNotes($token, $filter, 0, 200);
-
-        $notes = (array) $notes->notes;
+        /** @var NoteList $noteList */
+        $notes = (array) $noteList->notes;
 
         usort($notes, function($a, $b) {
             if ($a->attributes->reminderTime == $b->attributes->reminderTime) {
@@ -81,6 +92,6 @@ class DefaultController extends Controller
             return $a->attributes->reminderTime > $b->attributes->reminderTime ? 1 : -1;
         });
 
-        return ["notes" => $notes];
+        return $this->render(":default:schedule.html.twig", ["notes" => $notes]);
     }
 }
